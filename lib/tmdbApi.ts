dec599d339784e64091f424060dd53feef09273e
roirepus <rajsinsinwar159@gmail.com>
2025-05-20 17:31:25 +0530

    build fails

--- lib/tmdbApi.ts
+++ lib/tmdbApi.ts
@@ -11,7 +11,7 @@ export interface featuredMovieDetails {
 // export interface featuredApiRes{
 //   results:featuredMovieDetails[],
 // }
-export async function getFeaturedMovie(): Promise<featuredMovieDetails> {
+export async function getFeaturedMovie(): Promise<featuredMovieDetails | unknown> {
   //the api call return an array of movies
   const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   const options = {
@@ -21,17 +21,22 @@ export async function getFeaturedMovie(): Promise<featuredMovieDetails> {
       Authorization: config.env.tmdb.tmdbAuthHeader!
     }
   };
+  try {
 
-  const res = await fetch(url, options)
-  const data = await res.json();
-  if (!data.results || data.results.length === 0) {
-    throw new Error("Error getting featured movie details");
+    const res = await fetch(url, options)
+    const data = await res.json();
+    if (!data.results || data.results.length === 0) {
+      return null;
+    }
+    const featuredMovie = data.results[0];
+    return featuredMovie;
+  } catch (error) {
+    console.error("Failed to fetch featured movie details:", error);
+    return null;
   }
-  const featuredMovie = data.results[0];
-  return featuredMovie;
 }
 
-export async function getTrendingMovies(): Promise<featuredMovieDetails[]> {
+export async function getTrendingMovies(): Promise<featuredMovieDetails[] | unknown> {
   const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
   const options = {
     method: 'GET',
@@ -41,11 +46,17 @@ export async function getTrendingMovies(): Promise<featuredMovieDetails[]> {
 
     }
   };
+  try {
+    const res = await fetch(url, options);
+    const data = await res.json();
+    if (!data.results || data.results.length === 0) return null;
+    return data.results;
+  } catch (error) {
 
-  const res = await fetch(url, options);
-  const data = await res.json();
-  if (!data.results || data.results.lenght === 0) throw new Error("error getting trending movies",)
-  return data.results;
+    console.error("Failed to fetch featured movie details:", error);
+    return null;
+  }
+
 }
 
 export interface tvShowsDetails {
@@ -54,7 +65,7 @@ export interface tvShowsDetails {
   title: string,
   vote_average: number,
 }
-export async function getTrendingTVShows(): Promise<tvShowsDetails[] | undefined> {
+export async function getTrendingTVShows(): Promise<tvShowsDetails[] | unknown> {
   const url = 'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1';
   const options = {
     method: 'GET',
@@ -64,9 +75,15 @@ export async function getTrendingTVShows(): Promise<tvShowsDetails[] | undefined
     }
   };
 
-  const res = await fetch(url, options);
-  if (!res.ok) throw new Error("error getting tv shows")
-  const data = await res.json();
-  if (data.results && data.results.length != 0)
-    return data.results;
+  try {
+    const res = await fetch(url, options);
+    if (!res.ok) throw new Error("error getting tv shows")
+    const data = await res.json();
+    if (!data.result || data.result.lenght === 0) {
+      return null;
+    }
+  } catch (error) {
+    console.error("Failed to fetch featured movie details:", error);
+    return null;
+  }
 }
