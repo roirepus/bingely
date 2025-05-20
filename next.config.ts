a0cb04acaaae39cfa63a03274607d1efd48d80aa
roirepus <rajsinsinwar159@gmail.com>
2025-05-20 20:55:35 +0530

    cors

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [new URL("https://image.tmdb.org/**")]
-  }
-};
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/*',
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
 
+        ]
+      }
+    ]
+  }
+}
 export default nextConfig;
