1fa24d58303d6211fe21aed956729f0f4f342352
roirepus <rajsinsinwar159@gmail.com>
2025-05-20 20:58:01 +0530

    prod is tedious

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/api/*',
+        source: '/api/:path*',
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Credentials", value: "true" },
