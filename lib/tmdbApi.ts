479c68488a4c38cfc1c6576fd11209d8fda5ccc9
roirepus <rajsinsinwar159@gmail.com>
2025-05-20 19:44:17 +0530

    will it build?

--- lib/tmdbApi.ts
+++ lib/tmdbApi.ts
@@ -11,7 +11,7 @@ export interface featuredMovieDetails {
 // export interface featuredApiRes{
 //   results:featuredMovieDetails[],
 // }
-export async function getFeaturedMovie(): Promise<featuredMovieDetails | unknown> {
+export async function getFeaturedMovie(): Promise<featuredMovieDetails> {
   //the api call return an array of movies
   const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   const options = {
@@ -26,17 +26,17 @@ export async function getFeaturedMovie(): Promise<featuredMovieDetails | unknown
     const res = await fetch(url, options)
     const data = await res.json();
     if (!data.results || data.results.length === 0) {
-      return null;
+      return {} as featuredMovieDetails;
     }
     const featuredMovie = data.results[0];
     return featuredMovie;
   } catch (error) {
     console.error("Failed to fetch featured movie details:", error);
-    return null;
+    return {} as featuredMovieDetails;
   }
 }
 
-export async function getTrendingMovies(): Promise<featuredMovieDetails[] | unknown> {
+export async function getTrendingMovies(): Promise<featuredMovieDetails[]> {
   const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
   const options = {
     method: 'GET',
@@ -49,12 +49,12 @@ export async function getTrendingMovies(): Promise<featuredMovieDetails[] | unkn
   try {
     const res = await fetch(url, options);
     const data = await res.json();
-    if (!data.results || data.results.length === 0) return null;
+    if (!data.results || data.results.length === 0) return [] as featuredMovieDetails[];
     return data.results;
   } catch (error) {
 
     console.error("Failed to fetch featured movie details:", error);
-    return null;
+    return [] as featuredMovieDetails[];
   }
 
 }
@@ -65,7 +65,7 @@ export interface tvShowsDetails {
   title: string,
   vote_average: number,
 }
-export async function getTrendingTVShows(): Promise<tvShowsDetails[] | unknown> {
+export async function getTrendingTVShows(): Promise<tvShowsDetails[]> {
   const url = 'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1';
   const options = {
     method: 'GET',
@@ -79,11 +79,12 @@ export async function getTrendingTVShows(): Promise<tvShowsDetails[] | unknown> 
     const res = await fetch(url, options);
     if (!res.ok) throw new Error("error getting tv shows")
     const data = await res.json();
-    if (!data.result || data.result.lenght === 0) {
-      return null;
+    if (!data.results || data.results.length === 0) {
+      return [] as tvShowsDetails[];
     }
+    return data.results
   } catch (error) {
     console.error("Failed to fetch featured movie details:", error);
-    return null;
+    return [] as tvShowsDetails[];
   }
 }
