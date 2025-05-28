5286fad650a91d37f80209266d12a868e3ae42c4
roirepus <rajsinsinwar159@gmail.com>
2025-05-29 04:00:03 +0530

    prod is broken

--- app/api/debug/route.ts
+++ app/api/debug/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
-import config from "@/lib/config";
-export async function GET() {
-  return NextResponse.json({
-    // tmdbApiKey: process.env.TMDB_API_KEY,
-    tmdbAuthHeader: config.env.tmdb.tmdbAuthHeader,
-  });
-}
+// import { NextResponse } from "next/server";
+// import config from "@/lib/config";
+// export async function GET() {
+//   return NextResponse.json({
+//     // tmdbApiKey: process.env.TMDB_API_KEY,
+//     tmdbAuthHeader: config.env.tmdb.tmdbAuthHeader,
+//   });
+// }
